8b9a18f2d90b4f7aea871ea1c5b34fbff5236428
deepankar dey <deepankar.dey@techchefz.com>
2023-11-19 19:13:02 +0530

    changes

--- src/main/java/com/project/staragile/insureme/TestNGStar.java
+++ src/main/java/com/project/staragile/insureme/TestNGStar.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
-import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
